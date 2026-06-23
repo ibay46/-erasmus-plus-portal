8b1a2f8afc53f82ed20ee34cf37a009c14325e3b
Erasmus+ Portal <isabayraktar81@gmail.com>
2026-06-23 15:24:12 +0300

    Replace isomorphic-dompurify (jsdom/ESM incompatible on Vercel) with sanitize-html

--- lib/sanitize.ts
+++ lib/sanitize.ts
@@ -1,18 +1,15 @@
-import DOMPurify from "isomorphic-dompurify";
+import sanitize from "sanitize-html";
 
 export function sanitizeHtml(html: string): string {
-  return DOMPurify.sanitize(html, {
-    ADD_TAGS: ["iframe", "video"],
-    ADD_ATTR: [
-      "allow",
-      "allowfullscreen",
-      "frameborder",
-      "controls",
-      "src",
-      "target",
-      "rel",
-      "width",
-      "height",
-    ],
+  return sanitize(html, {
+    allowedTags: sanitize.defaults.allowedTags.concat(["img", "iframe", "video", "h1", "h2"]),
+    allowedAttributes: {
+      ...sanitize.defaults.allowedAttributes,
+      a: ["href", "name", "target", "rel"],
+      img: ["src", "alt", "width", "height"],
+      iframe: ["src", "allow", "allowfullscreen", "frameborder", "width", "height"],
+      video: ["src", "controls", "width", "height"],
+    },
+    allowedIframeHostnames: ["www.youtube.com", "youtube.com", "player.vimeo.com"],
   });
 }
